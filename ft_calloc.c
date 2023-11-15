e93126214a8ffdaf7c720d2ea4b2dc2faae586b9
Mouad <mowdl@Mouads-MacBook-Pro.local>
2023-11-15 22:47:31 +0100

    Protected against overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mel-meka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 16:53:37 by mel-meka          #+#    #+#             */
-/*   Updated: 2023/11/11 23:21:57 by mel-meka         ###   ########.fr       */
+/*   Updated: 2023/11/15 22:46:49 by mel-meka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*r;
 
+	if (size != 0 && count >= SIZE_MAX / size)
+		return (NULL);
 	r = malloc(count * size);
 	if (r == 0)
 		return (r);
